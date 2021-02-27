852b0163f46b424aa2e0e0b54f2614ceae526f35
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-02-27 23:10:08 +0100

    Add files via upload

---
+++ threeWayCountDown.js
@@ -0,0 +1,29 @@
+// Boucle 1
+
+let n1 = 101
+
+while (n1 > 0) {
+  console.log(n1)
+  n1 -= 2
+}
+
+// Boucle 2
+
+let n2 = 101
+do {
+
+  console.log(n2)
+  n2 -= 2
+
+} while (n2 > 0)
+
+console.log(n2)
+
+// Boucle 3
+
+for (let i = 101; i > 0; i -= 2) {
+  if (i % 2 === 0) {
+
+  }
+  console.log(i)
+}
