4d695186fe542d76782ccac04d683380b0e636e3
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-02-28 22:46:09 +0100

    Add files via upload

---
+++ onlyMultipleOf3And7.js
@@ -0,0 +1,18 @@
+//un programme qui affiche les multiples de 3
+
+for (let i = 3; i <= 1000; i++) {
+
+  if (i % 3 === 0) {
+    console.log(i)
+  }
+}
+
+
+//un programme qui affiche les multiples de 7
+
+for (let j = 7; j <= 1000; j++) {
+
+  if (j % 7 === 0) {
+    console.log(j)
+  }
+}
